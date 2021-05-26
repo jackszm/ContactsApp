4a1a7a69b91c807fdf9bf60fe184d7cc1d8b0f26
Jacek Szmelter <jacek.szmelter.ext@qonto.com>
2021-05-26 10:59:40 +0200

    Remove caching interceptors.

--- app/src/main/java/com/jsz/contactsapp/common/api/ApiModule.kt
+++ app/src/main/java/com/jsz/contactsapp/common/api/ApiModule.kt
@@ -1,7 +1,6 @@
 package com.jsz.contactsapp.common.api
 
 import android.content.Context
-import com.jsz.contactsapp.data.user.api.UsersApiService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.Cache
@@ -13,6 +12,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 val apiModule = module {
+
     fun provideCache(context: Context): Cache {
         val cacheSize = (5 * 1024 * 1024).toLong() // 5 MB
         return Cache(context.cacheDir, cacheSize)
@@ -20,43 +20,14 @@ val apiModule = module {
 
     fun provideHttpClient(
         cache: Cache,
-//    responseCachingInterceptor: Interceptor,
-//    offlineCachingInterceptor: Interceptor,
         loggingInterceptor: HttpLoggingInterceptor,
     ) = OkHttpClient.Builder()
         .cache(cache)
         .addInterceptor(loggingInterceptor)
         .build()
 
-// fun provideResponseCachingInterceptor() = Interceptor { chain ->
-//    val originalResponse: Response = chain.proceed(chain.request())
-//    originalResponse.newBuilder()
-//        .removeHeader("Pragma")
-//        .header("Cache-Control", "public, max-age=" + 5000)
-//        .build()
-//}
-//
-// fun provideOfflineCachingInterceptor(context: Context) = Interceptor { chain ->
-//    var request: Request = chain.request()
-//    if (!hasNetwork(context)) {
-//        request = request.newBuilder()
-//            .removeHeader("Pragma")
-//            .header("Cache-Control", "public, only-if-cached")
-//            .build()
-//    }
-//    chain.proceed(request)
-//}
-//
-//    fun provideHasNetwork(context: Context): Boolean {
-//        val connectivityManager =
-//            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-//        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
-//        return activeNetwork != null && activeNetwork.isConnected
-//    }
-
     fun provideLoggingInterceptor() = HttpLoggingInterceptor().apply {
         setLevel(HttpLoggingInterceptor.Level.NONE)
-//        setLevel(HttpLoggingInterceptor.Level.BODY)
     }
 
     fun provideMoshi() = Moshi.Builder()
@@ -74,15 +45,11 @@ val apiModule = module {
         .build()
 
 
-//    single { offlineCachingInterceptor(context = get()) }
-
     single { provideCache(context = get()) }
 
     single {
         provideHttpClient(
             cache = get(),
-//            responseCachingInterceptor = provideResponseCachingInterceptor(),
-//            offlineCachingInterceptor = get(),
             loggingInterceptor = provideLoggingInterceptor()
         )
     }
@@ -92,9 +59,5 @@ val apiModule = module {
             client = get(),
             moshi = provideMoshi()
         )
-    }
-
-    single {
-        get<Retrofit>().create(UsersApiService::class.java)
     }
 }
